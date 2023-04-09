02bca939b14f0e12bcc271b50b3922878e2ccebe
Vincent Luu <luuvl@student.unimelb.edu.au>
2023-04-10 09:36:42 +1000

    Test if without Task4 still works

--- src/main.c
+++ src/main.c
@@ -48,135 +48,144 @@ typedef struct stats {
 stats_t SJF(FILE* f, int q, char* memStrat);
 stats_t RR(FILE* f, int q, char* memStrat);
 int roundq(int time, int quantum);
+// char* toBigEndian(int number);
 void statsUpdate(int time, stats_t* stats, process_t* proc);
 void statsFinalise(int time, stats_t* stats);
 
 int main(int argc, char* argv[]) {
 
-    // char* filepath = NULL;
-    // char* scheduler = NULL;
-    // char* memStrat = NULL;
-    // int quantum;
+    char* filepath = NULL;
+    char* scheduler = NULL;
+    char* memStrat = NULL;
+    int quantum;
     
-    // // Read in settings
-    // int c;
-    // while ((c = getopt(argc, argv, ":f:s:m:q:")) != -1) {
-    //     switch(c)  {
-    //         case 'f':
-    //             filepath = optarg;
-    //             break;
-    //         case 's':
-    //             scheduler = optarg;
-    //             break;
-    //         case 'm':
-    //             memStrat = optarg;
-    //             break;
-    //         case 'q':
-    //             quantum = atoi(optarg);
-    //             break;
-    //     }
-    // }
+    // Read in settings
+    int c;
+    while ((c = getopt(argc, argv, ":f:s:m:q:")) != -1) {
+        switch(c)  {
+            case 'f':
+                filepath = optarg;
+                break;
+            case 's':
+                scheduler = optarg;
+                break;
+            case 'm':
+                memStrat = optarg;
+                break;
+            case 'q':
+                quantum = atoi(optarg);
+                break;
+        }
+    }
 
     // printf("file: %s\nscheduler: %s\nmemstrat: %s\nquantum: %d\n", filepath, scheduler, memStrat, quantum);
 
-    // FILE* f = fopen(filepath, "r");
-    // assert(f);
+    FILE* f = fopen(filepath, "r");
+    assert(f);
 
-    // stats_t stats;
+    stats_t stats;
 
-    // // Choose scheduler
-    // if (strcmp(scheduler, "SJF") == 0) {
-    //     stats = SJF(f, quantum, memStrat);
-    // } else if (strcmp(scheduler, "RR") == 0) {
-    //     stats = RR(f, quantum, memStrat);
-    // }
-    
-    // // Print stats
-    // printf("Turnaround time %d\n"
-    //        "Time overhead %.2lf %.2lf\n"
-    //        "Makespan %d\n",
-    //        stats.avgTurnaround, roundf(stats.maxOverhead*100)/100,
-    //        roundf(stats.avgOverhead*100)/100, stats.makespan);
-
-    // fclose(f);
-
-    // return 0;
-
-    int inputPipe[2];
-    assert(pipe(inputPipe) == 0);
-
-    int outputPipe[2];
-    assert(pipe(outputPipe) == 0);
-
-    pid_t pid = fork();
-    assert(pid != -1);
-
-    // This is the child process
-    if (pid == 0) {
-        
-        // Close write end of input pipe
-        close(inputPipe[1]);
-
-        // Duplicate read end of pipe to stdin
-        dup2(inputPipe[0], STDIN_FILENO);
-
-        // Close read end of input pipe
-        close(inputPipe[0]);
-
-
-        // Close read end of output pipe
-        close(outputPipe[0]);
-
-        // Duplicate write end of pipe to stdout
-        dup2(outputPipe[1], STDOUT_FILENO);
-
-        // Close write end of output pipe
-        close(outputPipe[1]);
-
-        // Execute child process
-        char *args[] = {"./process", "P", "-v", NULL};
-        execvp(args[0], args);
-
-        // execvp only returns if there was an error
-        perror("execvp");
-        exit(EXIT_FAILURE);
-
-    // This is the parent process
-    } else {
-        
-        // Close read end of input pipe
-        close(inputPipe[0]);
-
-        // Close write end of output pipe
-        close(outputPipe[1]);
-
-        // Send data to child process
-        char *data = "\x00\x00\x02\x80";
-        write(inputPipe[1], data, 4);
-
-        // Read data from child process
-        char outputBuffer[1024];
-        read(outputPipe[0], outputBuffer, 1);
-        printf("Received from child process: %c\n", outputBuffer[0]);
-
-        // Write data to child process
-        data = "\x00\x00\x02\xBC";
-        write(inputPipe[1], data, 4);
-        
-        // End ./process
-        kill(pid, SIGTERM);
-
-        // Read the hash
-        read(outputPipe[0], outputBuffer, 64);
-        printf("Received from child process: %.64s\n", outputBuffer);        
-
-        int status= 1;
-        wait(&status);
-        printf("Child process exited with status %d\n", status);
-        
+    // Choose scheduler
+    if (strcmp(scheduler, "SJF") == 0) {
+        stats = SJF(f, quantum, memStrat);
+    } else if (strcmp(scheduler, "RR") == 0) {
+        stats = RR(f, quantum, memStrat);
     }
+    
+    // Print stats
+    printf("Turnaround time %d\n"
+           "Time overhead %.2lf %.2lf\n"
+           "Makespan %d\n",
+           stats.avgTurnaround, roundf(stats.maxOverhead*100)/100,
+           roundf(stats.avgOverhead*100)/100, stats.makespan);
+
+    fclose(f);
 
     return 0;
+
+    // int inputPipe[2];
+    // assert(pipe(inputPipe) == 0);
+
+    // int outputPipe[2];
+    // assert(pipe(outputPipe) == 0);
+
+    // pid_t pid = fork();
+    // assert(pid != -1);
+
+    // // This is the child process
+    // if (pid == 0) {
+        
+    //     // Close write end of input pipe
+    //     close(inputPipe[1]);
+
+    //     // Duplicate read end of pipe to stdin
+    //     dup2(inputPipe[0], STDIN_FILENO);
+
+    //     // Close read end of input pipe
+    //     close(inputPipe[0]);
+
+
+    //     // Close read end of output pipe
+    //     close(outputPipe[0]);
+
+    //     // Duplicate write end of pipe to stdout
+    //     dup2(outputPipe[1], STDOUT_FILENO);
+
+    //     // Close write end of output pipe
+    //     close(outputPipe[1]);
+
+    //     // Execute child process
+    //     char *args[] = {"./process", "P1", "-v", NULL};
+    //     execvp(args[0], args);
+
+    //     // execvp only returns if there was an error
+    //     perror("execvp");
+    //     exit(EXIT_FAILURE);
+
+    // // This is the parent process
+    // } else {
+        
+    //     // Close read end of input pipe
+    //     close(inputPipe[0]);
+
+    //     // Close write end of output pipe
+    //     close(outputPipe[1]);
+
+    //     // Send data to child process
+    //     char *data = toBigEndian(0);
+    //     write(inputPipe[1], data, 4);
+
+    //     // Read data from child process
+    //     char outputBuffer[1024];
+    //     read(outputPipe[0], outputBuffer, 1);
+    //     printf("Received from child process: %c\n", outputBuffer[0]);
+
+    //     for (int time = 1; time < 50; time++) {
+    //         data = toBigEndian(time);
+    //         write(inputPipe[1], data, 4);
+    //         kill(pid,SIGCONT);
+    //         read(outputPipe[0], outputBuffer, 64);
+    //         printf("Received from child process: %.64s\n", outputBuffer);
+    //     }
+
+    //     // Write data to child process
+    //     data = toBigEndian(50);
+    //     write(inputPipe[1], data, 4);
+        
+    //     // End ./process
+    //     kill(pid, SIGTERM);
+
+    //     // Read the hash
+    //     read(outputPipe[0], outputBuffer, 64);
+    //     printf("Received from child process: %.64s\n", outputBuffer);        
+
+    //     int status= 1;
+    //     wait(&status);
+    //     printf("Child process exited with status %d\n", status);
+        
+    // }
+
+    // return 0;
 
 }
 
@@ -185,6 +194,23 @@ int roundq(int time, int quantum) {
     return (time%quantum == 0) ? time : (time/quantum+1)*quantum;
 }
 
+// char* toBigEndian(int number) {
+
+//     int* p = &number;
+
+//     char* bytePtr = (char*)p;
+
+//     // 4 byte Endian
+//     char* bytes = malloc(4);
+
+//     for (int i = 0; i < 4; i++) {
+//         bytes[i] = bytePtr[3-i];
+//         fprintf(stdout,"%x\n", bytes[i]);
+//     }
+
+//     return bytes;
+
+// }
 
 
 
@@ -321,10 +347,6 @@ stats_t RR(FILE* f, int q, char* memStrat) {
 
 
 
-
-
-
-
 stats_t SJF(FILE* f, int q, char* memStrat) {
 
     // Initialise structs
@@ -339,6 +361,13 @@ stats_t SJF(FILE* f, int q, char* memStrat) {
     }
 
     stats_t stats = {0};
+
+    // // Set up pipes
+    // int inputPipe[2];
+    // assert(pipe(inputPipe) == 0);
+
+    // int outputPipe[2];
+    // assert(pipe(outputPipe) == 0);
 
     // Set the time to the start quantum of the first process
     process_t* nextProc = processRead(f);
