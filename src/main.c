e9521d7f1ffe7216ea0b93af16c5506916a9fc4d
Vincent Luu <luuvl@student.unimelb.edu.au>
2023-04-13 18:01:59 +1000

    Valgrind CI test again

--- src/main.c
+++ src/main.c
@@ -83,7 +83,7 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    if (strcmp("cases/task2/simple.txt", filepath) == 0) {
+    if (strcmp("/cases/task2/simple.txt", filepath) == 0) {
         char* test = malloc(123);
         test[10] = 'a';
         return 0;
